67a2771efc9d1d3ee17619608eed7a6db8f5d645
Ahmed Abd El-Monem <112562093+Amon3m@users.noreply.github.com>
2022-11-13 17:56:50 +0200

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,8 +1,6 @@
-package oob;
-
 import java.util.Scanner;
 
-public class PrimeNumber {
+public class Main {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.println("Enter number ");
